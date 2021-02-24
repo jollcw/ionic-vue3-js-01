080d4231549a0a8943093004d9a3743adee630ef
jordi Olmedillas <jordi.olmedillas@gmail.com>
2021-02-25 00:46:33 +0100

    Pag originals

---
+++ src/pages/MemoriesPage.vue
@@ -0,0 +1,42 @@
+<template>
+  <base-layout page-title="All Memories">
+    <ion-list>
+      <ion-item router-link="/memories/1">Item 1</ion-item>
+      <ion-item>Item 2</ion-item>
+      <ion-item>Item 3</ion-item>
+    </ion-list>
+  </base-layout>
+  <!-- <ion-header>
+      <ion-toolbar>
+        <ion-title> Memories Page </ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content>
+     
+    </ion-content> -->
+</template>
+<script>
+import {
+  //   IonPage,
+  //   IonHeader,
+  //   IonToolbar,
+  //   IonTitle,
+  //   IonContent,
+  IonList,
+  IonItem,
+  IonButton,
+} from "@ionic/vue";
+export default {
+  // props: ["pageTitle"],
+  components: {
+    // IonPage,
+    // IonHeader,
+    // IonToolbar,
+    // IonTitle,
+    // IonContent,
+    IonList,
+    IonItem,
+    IonButton,
+  },
+};
+</script>
